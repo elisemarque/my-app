0030de180d384e1784b7f898996db35a464d4e50
Elise Marqué <elise.marque@etu.unistra.fr>
2023-04-23 22:47:10 +0200

    add code

---
+++ src/Components/Router.js
@@ -0,0 +1,17 @@
+// import {createHashRouter } from 'react-router-dom';
+
+// import About from './About';
+// import Main from './Main';
+
+// const router=createHashRouter([
+//     {
+//         path: '/',
+//         element:<Main />,
+//         children: [
+//             { path: '/', element: <Main /> },
+//             { path: '/about', element: <About /> }
+//         ]
+//     }
+// ])
+
+// export default router;
